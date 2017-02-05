651f0731041281ba1fa7fcc9fe9714c1f210a6b0
Derek <dschacht@gmail.com>
2017-02-05 15:34:45 -0500

    Updated to make the header on the weather pages come from a common area. Added Rainfall as a graph. Added a whole new page for indoor conditions. Added the ability to change the timespan that the graph covers.

---
+++ scripts/header.js
@@ -0,0 +1,20 @@
+function header() {
+  return "<table width='80%' align=center>" +
+         "<tr><td>" +
+         "<a href='index.html'>Home</a>" + 
+         "</td><td align=center>" +
+         "<a href='indoor.html'>Indoor Conditions</a> | "+
+         "<a href='weather.html'>Current Weather</a> | "+
+         "<a href='status.html'>Station Status</a>" + 
+         "</td><td align=right>" +
+         "<a href='https://github.com/astroeng/Weather_Station'>Station Code</a> | " +
+         "<a href='https://github.com/astroeng/Website'>Website Code</a>" + 
+         "</td></tr>" +
+         "<tr><td> </td>" +
+         "<td align=center>" +
+         "<a href='https://data.sparkfun.com/easy_weather2_indoor'>Raw Indoor Data</a> | " +
+         "<a href='https://data.sparkfun.com/easy_weather2'>Raw Weather Data</a> | " +
+         "<a href='https://data.sparkfun.com/easy_weather2_status'>Raw Status Data</a>" +
+         "</td></tr>" +
+         "</table>";
+}
